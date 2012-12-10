8fe7ab3b87314de88b4ba11184b9608c6d734181
jeffhuys <jeffhuys@gmail.com>
2012-12-10 14:44:20 +0100

    Nog niets

---
+++ src/ads_5_keuze_2/ADS_5_Keuze_2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ads_5_keuze_2;
+
+/**
+ *
+ * @author Jeff
+ */
+public class ADS_5_Keuze_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
